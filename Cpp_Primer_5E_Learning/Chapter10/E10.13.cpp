637ddee90569709f8b146e89a671242219826099
F <feiwofeifeixiaowo@gmail.com>
2016-10-20 10:22:15 +0800

    update

--- Cpp_Primer_5E_Learning/Chapter10/E10.13.cpp
+++ Cpp_Primer_5E_Learning/Chapter10/E10.13.cpp
@@ -3,15 +3,25 @@
 //
 #include <iostream>
 #include <string>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
+bool predict(const string &s1)
+{
+    return s1.size() >= 5;
+}
+
 int main()
 {
     string s("abcdefg");
-    bool predict = false;
-    //
-    partition(s.begin(),s.end(),predict);
+    vector<string> svec{"abcd","asdad","hello","world","whate"};
+
+    auto p_result = partition(svec.begin(),svec.end(),predict);
+
+    for(auto it = svec.cbegin(); it!= p_result;++it)
+        cout << *it << "\t";
+    cout << endl;
     return 0;
 }
