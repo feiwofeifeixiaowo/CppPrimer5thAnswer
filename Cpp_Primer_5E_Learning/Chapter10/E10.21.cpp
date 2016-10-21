788c0d7b1730e7cb63ab127484497aaf7499bc9c
F <feiwofeifeixiaowo@gmail.com>
2016-10-21 09:49:21 +0800

    update

--- Cpp_Primer_5E_Learning/Chapter10/E10.21.cpp
+++ Cpp_Primer_5E_Learning/Chapter10/E10.21.cpp
@@ -7,5 +7,20 @@ using namespace std;
 
 int main()
 {
+    int a = 3;
+    auto f = [&]() -> bool {
+        if(a == 0)
+            return true;
+        else
+        {
+            --a;
+            return false;
+        }
+    };
+
+    while(!f())
+        cout << a << "\t";
+    cout << endl;
+
     return 0;
 }
