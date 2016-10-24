61d4745b0ee002d54b4629fc35a1ccb2bbb23f2f
F <feiwofeifeixiaowo@gmail.com>
2016-10-24 16:47:11 +0800

    update

--- Cpp_Primer_5E_Learning/Chapter11/E11.4.cpp
+++ Cpp_Primer_5E_Learning/Chapter11/E11.4.cpp
@@ -3,9 +3,21 @@
 //
 #include <iostream>
 #include <map>
+#include <algorithm>
 #include <string>
+#include <cctype>
 
 using namespace std;
+
+auto strip(string &str) -> string const&
+{
+    //letter to lower
+    for(auto &ch : str) ch = tolower(ch);
+    //remove punct
+    str.erase(remove_if(str.begin(),str.end(),
+                        [](char &ch){return ispunct(ch);}),str.end());
+    return str;
+}
 
 int main()
 {
@@ -13,10 +25,13 @@ int main()
     string word;
     while(cin >> word)
     {
-        ++word_count[word];
+        //录入到map中
+        ++word_count[strip(word)];
+        //退出while
         if(word_count.size() > 5)
             break;
     }
+    //输出
     for(const auto &w : word_count)//w is a pair
         cout << w.first << " occurs " << w.second << ((w.second > 1) ? " times" : " time") << endl;
 
