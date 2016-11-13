d07f52ef50149269de84298ab00ad7dc4e5085c3
F <feiwofeifeixiaowo@gmail.com>
2016-11-14 00:59:22 +0800

    update

--- Cpp_Primer_5E_Learning/Chapter16/E16.12/E16.12.cpp
+++ Cpp_Primer_5E_Learning/Chapter16/E16.12/E16.12.cpp
@@ -2,3 +2,7 @@
 // Created by Xiyun on 2016/11/13.
 //
 
+int main()
+{
+    return 0;
+}
--- Cpp_Primer_5E_Learning/main.cpp
+++ Cpp_Primer_5E_Learning/main.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+int foo(int i, int j)
+{
+    return i + j;
+}
+
 int main() {
     cout << "Hello, World!" << endl;
+    int i = 2;
+
+    cout << "result is: " << ((i++) + (++i)) << endl;
+
+    // UB
+    cout << foo(i++,++i);
     return 0;
 }
+
