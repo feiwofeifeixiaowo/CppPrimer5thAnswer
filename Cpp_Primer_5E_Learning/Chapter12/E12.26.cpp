e3a0110a7e274fead282152471e0265f97a66185
F <feiwofeifeixiaowo@gmail.com>
2016-11-28 11:30:28 +0800

    update

--- Cpp_Primer_5E_Learning/Chapter12/E12.26.cpp
+++ Cpp_Primer_5E_Learning/Chapter12/E12.26.cpp
@@ -3,7 +3,28 @@
 //
 
 #include <iostream>
+#include <string>
+#include <memory>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::allocator;
 
 int main () {
+    const int n = 3;
+    allocator<string> alloc;
+    auto const p = alloc.allocate(n);
+    auto q = p;
+    string s;
+    while (q != p + n && cin >> s) {
+        alloc.construct(q++, s);
+    }
+    while (q != p) {
+        cout << *--q << " ";
+        alloc.destroy(q);
+    }
+    alloc.deallocate(p, n);
     return 0;
 }
--- Cpp_Primer_5E_Learning/main.cpp
+++ Cpp_Primer_5E_Learning/main.cpp
@@ -1,12 +1,16 @@
 // Copyright ® xy
 
 #include <iostream>
+#include <string>
+#include <memory>
 #include <limits>
 #include <cstdlib>
 
 using std::cout;
+using std::cin;
 using std::endl;
 using std::string;
+using std::allocator;
 using std::numeric_limits;
 
 int foo(int i, int j) {
@@ -27,6 +31,31 @@ int main() {
 //    cout << a << endl;
     cout << "str: " << s << endl;
     cout << "ret: " << ret << endl;
+
+    cout << "hello" "world" << endl;
+
+    // allocator test
+    string *const p = new string[10];
+    string s1;
+    string *q = p;
+    while (cin >> s1 && q != p + 10)
+        *q++ = s;
+    const size_t size = q - p;
+    delete[] p;
+
+    // 可以分配string 对象的alloc
+    allocator<string> alloc;
+    // 分配了10个未初始化的string
+    auto const p1 = alloc.allocate(10);
+    // q1 指向最后构造的元素之后的位置
+    auto q1 = p1;
+    // 对分配的内存进行构造
+    alloc.construct(q1++);
+    // 释放构造的string
+    while (q1 != p1)
+        alloc.destroy(--q1);    // destroy后，q1 指向最后一个构造的元素。
+     // 释放内存
+    alloc.deallocate(p, 10);
     return 0;
 }
 
