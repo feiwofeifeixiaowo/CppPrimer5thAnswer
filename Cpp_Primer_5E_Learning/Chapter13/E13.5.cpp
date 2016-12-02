dc81c22a0ec746cecdad9a32750eaf224d10ad79
F <feiwofeifeixiaowo@gmail.com>
2016-12-02 15:53:55 +0800

    add comments

--- Cpp_Primer_5E_Learning/Chapter13/E13.5.cpp
+++ Cpp_Primer_5E_Learning/Chapter13/E13.5.cpp
@@ -7,6 +7,8 @@ public:
     HasPtr(const std::string& s = std::string()) :
             ps(new std::string(s)), i(0) {}
     HasPtr(const HasPtr& rhs) : ps(new std::string(*rhs.ps)), i(rhs.i) { }
+    // 因为构造函数使用new，这里负责delete 释放
+    // 防止两个不同的对象指向同一个内存，执行深拷贝
     HasPtr& operator=(const HasPtr& rhs) {
         if (this != rhs) {
             std::string *temp_ps = new std::string(*rhs.ps);
