f5c68e6b66b0ec41d470a54a45f6bec251d1519f
F <feiwofeifeixiaowo@gmail.com>
2016-12-05 13:47:30 +0800

    update

--- Cpp_Primer_5E_Learning/Chapter13/E13.18/Employee.h
+++ Cpp_Primer_5E_Learning/Chapter13/E13.18/Employee.h
@@ -12,13 +12,15 @@ public:
 //    Employee() = default;
     Employee() {mysn_ = sn_number++;}
     Employee(const std::string& name) : name_(name), mysn_(sn_number++){}
-    int get_mysn() {return mysn_;}
     Employee(Employee& e) {name_ = e.name_; mysn_ = sn_number++;}
     Employee& operator=(Employee& rhs) {name_ = rhs.name_; return *this;}
+    int get_mysn() {return mysn_;}
+    const std::string& get_name() { return name_;}
 private:
     std::string name_;
     static int sn_number;
     int mysn_;
 };
+// 类的static 成员要在类外初始化
 int Employee::sn_number = 0;
 #endif //CPPPRIMER5ELEARNING_EMPLOYEE_H
